e4eba099c03585afbb02d503c01d2d9819c98a93
tazzledazzle <terenceschumacher@gmail.com>
2015-12-08 19:34:34 -0800

    completed full functionality for todo list

--- angular/js/todo.js
+++ angular/js/todo.js
@@ -14,12 +14,27 @@ todoListApp.config(function ($routeProvider){
 controllers['TodoController'] = function TodoController($scope){
 	$scope.todoList = [];
 	$scope.options = ["MUST", "SHOULD", "UNKNOWN", "COULD"];
-
+	$scope.id_index = 0;
 	$scope.addToList = function(){
 		var select = document.getElementById('selectOptions');
-		$scope.todoList.push({name: $scope.item.name, priority: select[select.selectedIndex].value});
+		$scope.todoList.push({id: $scope.id_index, name: $scope.item.name,
+			priority: select[select.selectedIndex].value, deleted: false});
+		$scope.id_index += 1;
 		$scope.item.name = "";
 		$scope.item.priority = "";
+	};
+
+	$scope.completeItem = function (){
+		var row_index = '.row_'+ this.i.id;
+		$(row_index).addClass('success');
+		$(row_index + ' #doneBtn').addClass('hidden');
+		$(row_index + ' #deleteBtn').addClass('hidden');
+	};
+
+	$scope.deleteItem = function () {
+		var row_index = '.row_'+ this.i.id;
+		$(row_index).addClass('hidden');
+		$scope.todoList[this.i.id].deleted = true;
 	};
 };
 
