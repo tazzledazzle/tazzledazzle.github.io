2430ca7aa19549cea921696feef55728520296eb
tazzledazzle <terenceschumacher@gmail.com>
2015-12-20 11:20:17 -0800

    data mining

---
+++ _posts/2015-12-20-guide-to-data-mining.md
@@ -0,0 +1,14 @@
+---
+layout: post
+title:  "Guide To Data Mining"
+date:   2015-12-20 11:16:42 -0800
+categories: machine_learning data data_science
+---
+* Manhattan Distance:
+* Euclidian Distance:
+* Minkowski Distance:
+* Pearson Correlation Coefficient:
+* Cosine Similarity:
+* K-Nearest Neighbors:
+
+
---
+++ angular/js/labs.js
@@ -0,0 +1,62 @@
+var LabApp = angular.module("labs", ['ngRoute']),
+    controllers = {};
+
+LabApp.config(function ($routeProvider){
+    $routeProvider
+        .when('/density', {
+            controller: 'densityController',
+            templateUrl: 'lab1Density.html'
+        })
+        .otherwise({ redirectTo: '/density'});
+});
+
+controllers["densityController"] = function densityController($scope) {
+    $scope.compute = function(){
+        debugger;
+        var data = $('#data').html();
+
+        function calculateDensity(obj) {
+
+        }
+
+        function displayMedianDensity(list) {
+
+        }
+
+        function displayCountry(obj) {
+
+        }
+
+        function traverseAndDisplay(list) {
+
+        }
+
+        function displayMostDense(list) {
+
+        }
+
+        function displayLeastDense(list) {
+
+        }
+
+        function displayTotalDensity(populationTotal, areaTotal) {
+
+        }
+
+        function calculateAverage(numerator, denominator) {
+
+        }
+
+        function displayArithmeticAverage(densityTotal, size) {
+
+        }
+
+        function moveEntries(list, insertPoint) {
+
+        }
+
+        function insertSorted(list)
+    }
+};
+
+LabApp.controller(controllers);
