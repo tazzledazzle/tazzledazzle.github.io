88fcc19e646a025a9586d080b5766bd9b5c5608c
tazzledazzle <terenceschumacher@gmail.com>
2015-12-28 10:04:21 -0800

    socket.io example (realtime-demo is broken :,( )

--- node/socketio/socketio1.js
+++ node/socketio/socketio1.js
@@ -1,1 +1,25 @@
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    fs = require('fs');
 
+app.listen(8098);
+console.log('server started on port 8098');
+function handler (req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('news', {content: 'news from server'});
+    socket.on('feedback', function (data) {
+        console.log(data);
+        socket.emit('news', {content: 'news - ' + new Date() });
+    });
+});
---
+++ node/socketio/socketio2.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    http = require('http');
+
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+console.log('server started');
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('news', {content: 'news from server'});
+    socket.on('feedback', function (data) {
+        console.log(data);
+        socket.emit('news', {content: 'news - ' + new Date() });
+    });
+});
+
+
+server.listen(8098);
