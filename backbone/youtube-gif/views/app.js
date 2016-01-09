a5315edda87a3aac8d73a3b3e30a18f11525006b
Imprev <imprev@Imprevs-MacBook-Pro.local>
2016-01-08 23:42:15 -0800

    phase 2 of youtube to gif completed, now for generation and download

--- backbone/youtube-gif/views/app.js
+++ backbone/youtube-gif/views/app.js
@@ -8,7 +8,7 @@ app.AppView = Backbone.View.extend({
 	events: {
 		'click #submitBtn' : '_onSearch',
 		'click input[name="loadMore"]' : '_onLoadMore',
-		'click a[href^="http://www.youtube.com"]': '_onVideoClick',
+		'click .video-image': '_onVideoClick',
 		'keyup ': '_onKeyUp'
 	},
 	initialize: function() {
@@ -81,7 +81,45 @@ app.AppView = Backbone.View.extend({
 		  }, this);
 	},
 	_onVideoClick: function (e) {
-		debugger;
-		e.preventDefault();
+		var queryString = e.target.getAttribute('alt').slice( e.target.getAttribute('alt').indexOf('?') + 1);
+		var queryVars = {"v": queryString.split("=")[1]};
+
+			// if GET variable "v" exists. This is the Youtube Video ID
+		if ( 'v' in queryVars )
+		{
+			// Prevent opening of external page
+			e.preventDefault();
+
+			// Variables for iFrame code. Width and height from data attributes, else use default.
+			var vidWidth = 560; // default
+			var vidHeight = 315; // default
+			if ( $(this).attr('data-width') ) { vidWidth = parseInt($(this).attr('data-width')); }
+			if ( $(this).attr('data-height') ) { vidHeight =  parseInt($(this).attr('data-height')); }
+			var iFrameCode = '<iframe width="' + vidWidth + '" height="'+ vidHeight +'" scrolling="no" allowtransparency="true" allowfullscreen="true" src="http://www.youtube.com/embed/'+  queryVars['v'] +'?rel=0&wmode=transparent&showinfo=0" frameborder="0"></iframe>';
+
+			// Replace Modal HTML with iFrame Embed
+			$('#mediaModal .modal-body').html(iFrameCode);
+			// Set new width of modal window, based on dynamic video content
+			$('#mediaModal').on('show.bs.modal', function () {
+				// Add video width to left and right padding, to get new width of modal window
+				var modalBody = $(this).find('.modal-body');
+				var modalDialog = $(this).find('.modal-dialog');
+				var newModalWidth = vidWidth + parseInt(modalBody.css("padding-left")) + parseInt(modalBody.css("padding-right"));
+				newModalWidth += parseInt(modalDialog.css("padding-left")) + parseInt(modalDialog.css("padding-right"));
+				newModalWidth += 'px';
+				// Set width of modal (Bootstrap 3.0)
+			    $(this).find('.modal-dialog').css('width', newModalWidth);
+			});
+
+			// Open Modal
+			$('#mediaModal').modal();
+		}
+
+
+		// Clear modal contents on close. 
+		// There was mention of videos that kept playing in the background.
+		$('#mediaModal').on('hidden.bs.modal', function () {
+			$('#mediaModal .modal-body').html('');
+		});
 	}
 });
