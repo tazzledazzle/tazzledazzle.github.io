6944e0fae7504002225df9560a6240e44fe80668
tazzledazzle <terenceschumacher@gmail.com>
2016-01-11 08:17:25 -0800

    instagram web application

---
+++ _posts/2016-01-07-Competitve-Programming.markdown
@@ -0,0 +1,16 @@
+---
+layout: post
+title:  "Competitive Programming Practice"
+date:   2015-11-06 13:19:42 -0800
+categories: algorithms data_structures
+---
+UVa 10550 - Combination Lock
+
+{% highlight ruby %}
+def print_msg(name)
+  puts "#{name} is Awesome!"
+end
+print_msg('Terence')
+#=> prints 'Terence is Awesome!' to STDOUT.
+{% endhighlight %}
+
--- backbone/youtube-gif/views/app.js
+++ backbone/youtube-gif/views/app.js
@@ -9,14 +9,13 @@ app.AppView = Backbone.View.extend({
 		'click #submitBtn' : '_onSearch',
 		'click input[name="loadMore"]' : '_onLoadMore',
 		'click .video-image': '_onVideoClick',
-		'keyup ': '_onKeyUp'
+		'keyup ': '_onKeyUp',
+		'click #generate': '_onClickGenerate'
 	},
 	initialize: function() {
 		this.$container = this.$('#video-collection');
-		// this.$videoData = this.grabVideos();
 		this.$videoData = [1,2,3,4,5];
-		// app.Videos.fetch();
-		
+
 		this.render();
 	},
 	render: function() {
@@ -90,15 +89,19 @@ app.AppView = Backbone.View.extend({
 			// Prevent opening of external page
 			e.preventDefault();
 
+			var template = _.template($('#modal-template').html());
+			$(this.$el).append(template);
+			$(this.$el).append('<script src="https://www.youtube.com/iframe_api"></script>');
 			// Variables for iFrame code. Width and height from data attributes, else use default.
 			var vidWidth = 560; // default
 			var vidHeight = 315; // default
 			if ( $(this).attr('data-width') ) { vidWidth = parseInt($(this).attr('data-width')); }
 			if ( $(this).attr('data-height') ) { vidHeight =  parseInt($(this).attr('data-height')); }
-			var iFrameCode = '<iframe width="' + vidWidth + '" height="'+ vidHeight +'" scrolling="no" allowtransparency="true" allowfullscreen="true" src="http://www.youtube.com/embed/'+  queryVars['v'] +'?rel=0&wmode=transparent&showinfo=0" frameborder="0"></iframe>';
-
+			//var iFrameCode = '<iframe width="' + vidWidth + '" height="'+ vidHeight +'" scrolling="no" allowtransparency="true" allowfullscreen="true" src="http://www.youtube.com/embed/'+  queryVars['v'] +'?rel=0&wmode=transparent&showinfo=0&enablejsapi=1&playerapiid=ytplayer&version=3" frameborder="0"></iframe>';
+			var SWFCode = '<embed id="my-video" style="height: 300px" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen allowfullscreen="true" allowscriptaccess="always" quality="high" bgcolor="#000000" name="my-video" style="" src="http://www.youtube.com/v/'+ queryVars['v'] +'?enablejsapi=1&version=3&playerapiid=ytplayer" type="application/x-shockwave-flash">';
 			// Replace Modal HTML with iFrame Embed
-			$('#mediaModal .modal-body').html(iFrameCode);
+			$('#mediaModal .modal-body').html(SWFCode);
+
 			// Set new width of modal window, based on dynamic video content
 			$('#mediaModal').on('show.bs.modal', function () {
 				// Add video width to left and right padding, to get new width of modal window
@@ -110,9 +113,20 @@ app.AppView = Backbone.View.extend({
 				// Set width of modal (Bootstrap 3.0)
 			    $(this).find('.modal-dialog').css('width', newModalWidth);
 			});
-
 			// Open Modal
 			$('#mediaModal').modal();
+			var ytplayer;
+
+			$('#generate').click(function () {
+				ytplayer = document.getElementById("my-video");
+				ytplayer.playVideo();
+				debugger;
+				$('#startTime').val(ytplayer.getCurrentTime());
+				//todo get duration
+				//todo actually generate on stop of video
+				//send over to https://giflayer.com/dashboard api
+
+			});
 		}
 
 
