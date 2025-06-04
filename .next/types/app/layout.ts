99d803224b77da157b3946df9a1994d8da58ec71
Terence Schumacher <terenceschumacher@gmail.com>
2025-06-03 19:41:42 -0700

    lets try this one out

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/terenceschumacher/Downloads/project/app/layout.tsx
+// File: /Users/terenceschumacher/IdeaProjects/tazzledazzle.github.io/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
